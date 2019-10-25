56d4bb6b897f1b899b1f70a2cff1ef9f541e2dec
L <L@163.com>
2019-10-25 12:30:39 +0800

    22

--- pro2/src/com/testDemo.java
+++ pro2/src/com/testDemo.java
@@ -1,7 +1,13 @@
 package com;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class testDemo {
     public static void main(String[] args) {
         System.out.println("hello git");
+    }
+
+    public void fun(){
+        System.out.println("222");
     }
 }
